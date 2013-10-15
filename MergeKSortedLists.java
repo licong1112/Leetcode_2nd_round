4ccf0653ec9560d5f03a9976dd789a81345b0576
Cong Li <licong1112@gmail.com>
2013-10-15 18:25:47 +0800

    Change format a bit

--- MergeKSortedLists.java
+++ MergeKSortedLists.java
@@ -2,18 +2,18 @@ import java.util.ArrayList;
 
 public class MergeKSortedLists {	
 	public ListNode mergeKLists(ArrayList<ListNode> lists) {
-        if (lists == null || lists.size() == 0) return null;
-		if (lists.size() == 1) return lists.get(0);
+		if (lists == null || lists.size() == 0) return null;
+        if (lists.size() == 1) return lists.get(0);
         ArrayList<ListNode> new_lists = new ArrayList<ListNode>();
         for (int i = 0; i < lists.size(); i += 2) {
         	ListNode newlist = new ListNode(0); // fake head
         	if (i != lists.size()-1) {
         		mergeTwoLists(lists.get(i), lists.get(i+1), newlist);
         		new_lists.add(newlist.next);
-        	} else {
-        		new_lists.add(lists.get(i));
-        	}
-        }
+    		} else {
+    			new_lists.add(lists.get(i));
+			}
+    	}
         return mergeKLists(new_lists);
     }
 	
