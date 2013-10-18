87a10a6f5b31c6b9c6cca0889bbb8b078cc50aa8
Cong Li <licong1112@gmail.com>
2013-10-18 15:21:29 +0800

    A little change

--- RemoveDuplicatesFromSortedArray.java
+++ RemoveDuplicatesFromSortedArray.java
@@ -2,8 +2,7 @@
 public class RemoveDuplicatesFromSortedArray {
 	public int removeDuplicates(int[] A) {
 		int position = (A.length == 0 ? 0 : 1);
-        int i = 1;
-        for (; i < A.length; ++i) {
+        for (int i = 1; i < A.length; ++i) {
         	if (A[i] != A[i-1]) {
         		A[position++] = A[i];
         	}
